70d480fa31886edf4acae9e3a8d00a7cc0d5bc75
Azusa <akiriin0@gmail.com>
2021-09-22 12:40:24 -0400

    Updated new GardenMode

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 //Mode.hpp declares the "Mode::current" static member variable, which is used to decide where event-handling, updating, and drawing events go:
 #include "Mode.hpp"
 
-//The 'PlayMode' mode plays the game:
-#include "PlayMode.hpp"
+//The 'GardenMode' mode plays the game:
+#include "GardenMode.hpp"
 
 //For asset loading:
 #include "Load.hpp"
@@ -112,7 +112,7 @@ int main(int argc, char **argv) {
 	call_load_functions();
 
 	//------------ create game mode + make current --------------
-	Mode::set_current(std::make_shared< PlayMode >());
+	Mode::set_current(std::make_shared< GardenMode >());
 
 	//------------ main loop ------------
 
